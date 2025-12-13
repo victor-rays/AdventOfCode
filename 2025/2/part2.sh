24c8cf8abe35755c7c6891885de5682c6b62751b
Victor-ray, S. <git@zendai.net.eu.org>
2025-12-13 18:01:20 +0100

    ࿓❯ Day 2.
    Finished part one and part two puzzles for the second day.
    
    Signed-off-by: Victor-ray, S. <git@zendai.net.eu.org>

--- 2025/2/part2.sh
+++ 2025/2/part2.sh
@@ -6,12 +6,23 @@ function check_number {
     local NUM="$1"
     local LEN="${#NUM}"
     local MID="$((LEN / 2))"
-    if ((LEN % 2 == 1)); then
-        return 0
-    fi
-    local HALF="${NUM:0:MID}"
-    local NEWNUM="$HALF$HALF"
-    (( NEWNUM != NUM ))
+    local i
+    local newstring
+    for ((i = 1; i <= MID; i++)); do
+        local substring="${NUM:0:i}"
+        if ((LEN % i == 0)); then
+            local amount=$((LEN / i))
+            local j
+            local newstring=''
+            for ((j = 0; j < amount; j++)); do
+                newstring+="$substring"
+            done
+            if ((newstring == NUM)); then
+                return 1
+            fi
+        fi
+    done
+    return 0
 }
 
 function main {
@@ -19,19 +30,19 @@ function main {
     local -a ID_RANGES
     IFS=',' read -r -a ID_RANGES < "$INPUT_FILE"
     local START END
-    local -i i part_one=0
+    local -i i part_two=0
     for x in "${ID_RANGES[@]}"; do
         IFS='-' read -r START END <<<"$x"
         echo "Range: $START → $END"
         for ((i = START; i <= END; i++)); do
             if ! check_number "$i"; then
                 echo "Invalid ID: $i"
-                (( part_one+=i ))
+                (( part_two+=i ))
             fi
         done
     done
-    # Part 1 correct answer is: 29818212493
-    echo "Part 1: $part_one"
+    # Part 2 correct answer is: 37432260594
+    echo "Part 2: $part_two"
 }
 
 main "$@"
