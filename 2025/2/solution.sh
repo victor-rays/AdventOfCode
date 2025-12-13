f023da4e4e89c5781d44ea16469f36b88a99e9d0
Victor-ray, S. <git@zendai.net.eu.org>
2025-12-13 16:48:35 +0100

    ࿓❯ Start day 2.
    Begin with the solution for day two.
    
    Signed-off-by: Victor-ray, S. <git@zendai.net.eu.org>

--- 2025/2/solution.sh
+++ 2025/2/solution.sh
@@ -4,7 +4,15 @@
 
 function main {
     local -r INPUT_FILE=input.txt
-    
+    local -a ID_RANGES
+    IFS=',' read -r -a ID_RANGES < "$INPUT_FILE"
+    local START END
+    for x in "${ID_RANGES[@]}"; do
+        START="$(cut -d '-' -f 1 <<<"$x")"
+        END="$(cut -d '-' -f 2 <<<"$x")"
+        echo "$START"
+        echo "$END"
+    done
 }
 
 main "$@"
